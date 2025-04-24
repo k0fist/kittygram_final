ac2b0ce6d6468db213a68f07fc2dcf8dfd97be67
Vadim Sirotkin <sirotkin.201515@gmail.com>
2025-04-24 14:51:57 +0500

    fix action

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,8 +1,11 @@
 # flake8: noqa
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(dotenv_path=BASE_DIR / '.env')
 
 SECRET_KEY = os.getenv('DJANGO_SECRET')
 
